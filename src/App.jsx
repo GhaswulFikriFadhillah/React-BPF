47edaee34af592b4fd283fc3185d7420a04faf41
GhaswulFikriFadhillah <ghaswul23ti@mahasiswa.pcr.ac.id>
2025-05-21 15:01:49 +0700

    Ubah Text

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Selamat Datang di React Git App
         </p>
       </div>
       <p className="read-the-docs">
